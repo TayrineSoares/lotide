c4bd9d3728761c061805ceb09a592bdc411cb7af
TayrineSoares <tayrinecristina@hotmail.com>
2024-10-26 13:45:42 -0400

    Equal Arrays function

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false; 
+  }
+  if (first.length === second.length) {
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
