ed7c6133f8008241844caa6b0eda9da342c95073
TayrineSoares <tayrinecristina@hotmail.com>
2024-10-26 13:52:35 -0400

    Included more tests and got back to my original code

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false; 
+  }
   if (first.length === second.length) {
     for (let i = 0; i < first.length; i++) {
       if (first[i] !== second[i]) {
@@ -17,5 +20,11 @@ const eqArrays = function(first, second) {
   }
   return true;
 }
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
