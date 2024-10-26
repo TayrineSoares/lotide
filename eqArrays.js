28e52ee807774ed5d6c857ca5eb7c48a61d3a7b2
TayrineSoares <tayrinecristina@hotmail.com>
2024-10-26 13:55:09 -0400

    Simplified code with compass suggestions

--- eqArrays.js
+++ eqArrays.js
@@ -11,13 +11,11 @@ const eqArrays = function(first, second) {
   if (first.length !== second.length) {
     return false; 
   }
-  if (first.length === second.length) {
     for (let i = 0; i < first.length; i++) {
       if (first[i] !== second[i]) {
-        return false;
+      return false;
       }
     }
-  }
   return true;
 }
 
