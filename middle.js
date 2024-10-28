e95b7fc48d9053d52331d7cb90b658fc636b0033
TayrineSoares <tayrinecristina@hotmail.com>
2024-10-28 16:18:33 -0400

    compass suggestions

--- middle.js
+++ middle.js
@@ -20,27 +20,6 @@ const assertArraysEqual = function(arr1, arr2) {
 
 assertArraysEqual([1, 2, 3], [1, 2,3]);
 
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const equalArrays = function(first, second) {
-  if (first.length !== second.length) {
-    return false; 
-  }
-    for (let i = 0; i < first.length; i++) {
-      if (first[i] !== second[i]) {
-      return false;
-      }
-    }
-  return true;
-}
-
 const middle = function(array) {
   const length = array.length;
 
@@ -61,4 +40,5 @@ console.log(middle([1, 2]));
 console.log(middle([1, 2, 3]));
 console.log(middle([1, 2, 3, 4, 5]));
 console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+console.log(middle([1, 2, 3, 4, 5, 6])); 
+assertArraysEqual(middle([1, 2, 3]), [2]);
