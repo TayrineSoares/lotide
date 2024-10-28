a6133e4d95460aff8715f33bbb0af282db66955c
TayrineSoares <tayrinecristina@hotmail.com>
2024-10-28 12:05:48 -0400

    Assert Array Equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("Assertion did not pass: The arrays are not equal.");
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2,3]);
