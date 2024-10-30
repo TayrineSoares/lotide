0e13257e6a99e1272ae6c50c61dc086c5d4b02f8
TayrineSoares <tayrinecristina@hotmail.com>
2024-10-30 17:48:21 -0400

    created for of loop

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,25 @@
 //Create a function called countLetters.
+//The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
 
-const countLetters = function () {
-  
+// For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
+
+// Before implementing this function, we need to think about how it will report back these counts. It's not that we want to know how many H's are in this sentence?, because that would just be a number. We need it report back multiple numbers.
+
+// take in the string 
+// loop through the string and identify letters 
+// increase the count and print letter + count 
+
+
+
+const countLetters = function (str) {
+  for (value of str) {
+    console.log(value);
+
+  }
+
+
 }
+
+const test = "Testando";
+
+console.log(countLetters(test));
