7e1416bb55117682483aa09d368f5313d0ccc7bb
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-04 11:52:33 -0500

    AssertEqual

--- findKey.js
+++ findKey.js
@@ -22,15 +22,18 @@ const findKey = function(object, callback) {
   }
   return undefined;
 };
-
-console.log(findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
+const testCase1 = {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+}
+console.log(findKey((testCase1),
   (x) => x.stars === 2
 )); // => "noma"
+
+
+// USING ASSERT EQUAL TO CHECK 
+assertEqual((findKey(testCase1,(x) => x.stars === 2)), "noma"); 
