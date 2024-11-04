7d402f2321fdedbee6e38e943ad641c66e8b83fe
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-04 11:22:05 -0500

    Updated code

--- takeUntil.js
+++ takeUntil.js
@@ -6,7 +6,14 @@
 // The callback (which Lodash calls "predicate")
 
 const takeUntil = function(array, callback) {
-
+  result = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+      result.push(item);
+  }
+  return result;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
@@ -18,5 +25,3 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
-
-///trying to push changes 
