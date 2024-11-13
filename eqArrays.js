bf365418f32507b445a430c97a4ddde5a659631d
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-13 14:41:24 -0500

    Refactored eqArrays and created eqArraysTest file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(first, second) {
   if (first.length !== second.length) {
@@ -18,7 +10,7 @@ const eqArrays = function(first, second) {
     }
   return true;
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+module.exports = eqArrays; 
