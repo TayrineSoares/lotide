9473a33685f181de840bd15aff71d1e4d299d385
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-13 14:35:53 -0500

    cleaned console logs on tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,15 +5,13 @@ const assertEqual = require("../assertEqual");
 //tests
 
 const words = (["Yo Yo", "Lighthouse", "Labs"]);
-console.log(tail(words));
 assertEqual(words.length, 3);
 
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
+
 
 const result1 = tail([1, 2, 3]);
-console.log(result1);
 
 assertEqual(tail(words).length, words.length - 1); //test that the tail function returns an array with the correct length, and that using an array with one or zero elements as an argument returns an empty array.
 
