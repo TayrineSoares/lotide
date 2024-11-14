5d3e2ad01863657d72ee90cc074c9cbba6ff5a24
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-14 13:25:09 -0500

    Refactored middleTest with Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,15 +2,28 @@
 // For arrays with odd number of elements, an array containing a single middle element should be returned.
 // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
 
-const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual"); 
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []); 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
