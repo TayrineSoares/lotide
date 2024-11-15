d02e32fcb63664a7b224fcf6b1ec56a14ce4947a
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-15 13:42:19 -0500

    Refactored and test file for takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,4 @@
-// We will be implementing a function quite similar to this one, but isn't present in their library: takeUntil.
-
-// It will take in two parameters as well:
+// The takeUntil function will take in two parameters:
 
 // The array to work with
 // The callback (which Lodash calls "predicate")
@@ -16,12 +14,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+module.exports = takeUntil; 
