1f94986df12bf53879b324cd935ce6e026a7da21
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-15 13:50:19 -0500

    Updated index

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const eqArrays            = require('./eqArrays');
 const eqObjects           = require('./eqObjects');
 const findKey             = require('./findKey');
 const findKeyByValue      = require('./findKeyByValue');
-const flatten             = require('./flatten');
 const head                = require('./head');
 const letterPositions     = require('./letterPositions');
 const map                 = require('./map');
 const middle              = require('./middle');
-const min                 = require('./min');
 const tail                = require('./tail');
 const takeUntil           = require('./takeUntil');
 const without             = require('./without');
@@ -31,12 +29,10 @@ module.exports = {
   eqObjects:          eqObjects,
   findKey:            findKey,
   findKeyByValue:     findKeyByValue,
-  flatten:            flatten,
   head:               head,
   letterPositions:    letterPositions,
   map:                map,
   middle:             middle,
-  min:                min,
   tail:               tail,
   takeUntil:          takeUntil,
   without:            without
